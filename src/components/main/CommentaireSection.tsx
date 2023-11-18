f66f9313e6a91655ea3be9f2e3de9fa96532c641
silverstone217 <stephuni35@gmail.com>
2023-11-18 17:34:22 +0300

    fix button comment

--- src/components/main/CommentaireSection.tsx
+++ src/components/main/CommentaireSection.tsx
@@ -46,6 +46,7 @@ const CommentaireSection: FC<Props> = async({blog}) => {
                 {
                     session?.user.id && blog?.id ?  <CommentForm session={session} id={blog?.id}/>
                     : <Link href="/" className='h-14 w-3/5 flex items-center justify-center p-2 text-xl 
+                        border border-black-rgb
                         hover:shadow-3xl transition-all duration-300 delay-150 ease-in rounded-md'>
                         Login to comment
                     </Link>
