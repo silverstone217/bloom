25e43117c4f1f01256bc6832e10779da9e7a09e7
silverstone217 <stephuni35@gmail.com>
2023-11-19 20:52:07 +0300

    add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+export default async function sitemap(){
+    return [
+        {
+            url : "https://bloom-three-flame.vercel.app",
+            lastModified : new Date()
+        },
+        {
+            url : "https://bloom-three-flame.vercel.app/home",
+            lastModified : new Date()
+        },
+        {
+            url : "https://bloom-three-flame.vercel.app/blog",
+            lastModified : new Date()
+        },
+        {
+            url : "https://bloom-three-flame.vercel.app/create",
+            lastModified : new Date()
+        },
+        {
+            url : "https://bloom-three-flame.vercel.app/myblog",
+            lastModified : new Date()
+        }
+    ]
+}
